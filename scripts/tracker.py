a1f13556d3f5926ced30bf5fd1fc9cbd9980e5b3
Jyrijoul <jyri.joul@gmail.com>
2021-05-07 12:20:08 +0300

    Cleaned up and documented the tracker code a bit.

--- scripts/tracker.py
+++ scripts/tracker.py
@@ -19,36 +19,40 @@ DEBUG = False
 TIME = True
 
 
-class WorldPoint:
-    def __init__(self, x=0, y=0, z=0, s_x=0, s_y=0, s_z=0, bounding_box=None):
+class BoundingBox:
+    def __init__(self, x=0, y=0, z=0, size_x=0, size_y=0, size_z=0, bounding_box=None):
+        """Create a bounding box with all values passed separately or using a BoundingBox3D message"""
+
         if not bounding_box:
             self.x = x
             self.y = y
             self.z = z
-            self.s_x = s_x
-            self.s_y = s_y
-            self.s_z = s_z
+            self.size_x = size_x
+            self.size_y = size_y
+            self.size_z = size_z
         else:
             self.x = bounding_box.center.position.x
             self.y = bounding_box.center.position.y
             self.z = bounding_box.center.position.z
-            self.s_x = bounding_box.size.x
-            self.s_y = bounding_box.size.y
-            self.s_z = bounding_box.size.z
+            self.size_x = bounding_box.size.x
+            self.size_y = bounding_box.size.y
+            self.size_z = bounding_box.size.z
 
 
 class DetectedObject:
-    def __init__(self, uid, world_point):
-        # Set initial values in order to call self.update()
-        self.x = world_point.x
-        self.y = world_point.y
-        
-        self.update(world_point)
+    def __init__(self, uid, bounding_box):
+        """Creates a detected object based on the bounding box and UID"""
 
+        # Set initial values in order to call self.update()
+        # because velocity is calculated based on the previous self.x and self.y.
+        self.x = bounding_box.x
+        self.y = bounding_box.y
+
+        self.update(bounding_box)
         self.uid = uid
 
         if DEBUG:
-            print("Created new object:", self)
+            rospy.loginfo("Created new object: " + str(self))
 
     def __eq__(self, other):
         return self.uid == other.uid
@@ -59,16 +63,16 @@ class DetectedObject:
     def __str__(self):
         return "UID: " + str(self.uid) + ", x: " + str(self.x) + ", y: " + str(self.y) + ", v_x: " + str(self.v_x) + ", v_y: " + str(self.v_y)
 
-    def update(self, world_point):
+    def update(self, bounding_box):
         """Updates the object's position, scale, calculates the velocity, resets disappeared counter."""
-        x, y = world_point.x, world_point.y
+        x, y = bounding_box.x, bounding_box.y
         self.v_x = x - self.x
         self.v_y = y - self.y
         self.x = x
         self.y = y
-        self.z = world_point.z
-        self.diameter = world_point.s_x
-        self.height = world_point.s_y
+        self.z = bounding_box.z
+        self.diameter = bounding_box.size_x
+        self.height = bounding_box.size_y
         self.disappeared = 0
 
     def has_disappeared(self):
@@ -76,84 +80,112 @@ class DetectedObject:
 
 
 class Tracker:
-    def __init__(self, max_frames_disappeared=30):
+    def __init__(self, max_frames_disappeared=30, starting_id=0):
+        """Initializes the tracker
+        
+        An optional disappearance threshold and starting ID for detections can be provided
+        """
+
         self.max_frames_disappeared = max_frames_disappeared
-        self.current_uid = 0
+        self.current_uid = starting_id
 
         self.objects = []
 
-    def new_object(self, world_point):
+    def new_object(self, bounding_box):
         """Register a newly detected object."""
-        self.objects.append(DetectedObject(self.current_uid, world_point))
+        self.objects.append(DetectedObject(self.current_uid, bounding_box))
 
-        # Increment the UID.
+        # Increment the UID for the next detected object.
         self.current_uid += 1
 
     def get_coordinates(self):
+        """Returns coordinates of detected objects in a Python list"""
+        # This can possibly be improved a bit with Numpy
         coordinates = []
         for detected_object in self.objects:
             if DEBUG:
-                print("Object:", detected_object)
+                rospy.loginfo("Object: " + str(detected_object))
             coordinates.append((detected_object.x, detected_object.y))
 
         return coordinates
 
     def delete_if_disappeared(self, detected_object):
-        """Delete the long-disappeared objects (>= max_frames_disappeared)"""
+        """Deletes the long-disappeared objects (when time disappeared >= max_frames_disappeared)"""
+        
         if detected_object.disappeared >= self.max_frames_disappeared:
             self.objects.remove(detected_object)
 
-    def update(self, world_points: list):
-        """Updates the positions of detected objects, add new objects and deletes old objects."""
-        if len(world_points) == 0:
+    def update(self, bounding_boxes: list):
+        """Updates the positions of detected objects, add new objects and deletes long-disappeared objects."""
+        
+        # If there are no detections, all of the previous objects have disappeared.
+        if len(bounding_boxes) == 0:
             for detected_object in self.objects:
                 detected_object.disappeared()
                 self.delete_if_disappeared(detected_object)
+        # If there are no existing objects, just register all the detections.
         elif len(self.objects) == 0:
-            for world_point in world_points:
-                self.new_object(world_point)
+            for bounding_box in bounding_boxes:
+                self.new_object(bounding_box)
+        # Otherwise, find the closest matches between existing objects and detections.
         else:
+            # Calculate the Euclidean distance between all the pairs of existing objects and detections.
+            # Only coordinates needed for scipy.spatial.distance.cdist()
             current_coordinates = self.get_coordinates()
+
+            # An example of the output of cdist():
+            #           new_1   new_2
+            # current_1   1       2
+            # current_2   3       4
             distances = distance.cdist(
-                np.array(current_coordinates), [(o.x, o.y) for o in world_points], "euclidean")
+                np.array(current_coordinates), [(bb.x, bb.y) for bb in bounding_boxes], "euclidean")
 
-            # The following part is from the following article:
+            # The following part is mainly from the following article:
             # https://www.pyimagesearch.com/2018/07/23/simple-object-tracking-with-opencv/
-            # Slightly modified the given code.
-            rows = distances.min(axis=1).argsort()
-            cols = distances.argmin(axis=1)[rows]
+            # Slightly modified the given code, but it was already pretty optimal (using Numpy).
+
             # Basically, this finds indices with least distances between the frames
+            # current == rows; new == columns;
+            indices_sorted_current = distances.min(axis=1).argsort()
+            indices_sorted_new = distances.argmin(
+                axis=1)[indices_sorted_current]
 
-            used_rows = set()
-            used_cols = set()
+            # Using set() to filter out the indices already used,
+            # also enables set difference operation for later
+            indices_used_current = set()
+            indices_used_new = set()
 
-            for i, j in zip(rows, cols):
-                if i in used_rows or j in used_cols:
+            for i, j in zip(indices_sorted_current, indices_sorted_new):
+                if i in indices_used_current or j in indices_used_new:
                     continue
                 else:
-                    self.objects[i].update(world_points[j])
-                    used_rows.add(i)
-                    used_cols.add(j)
+                    self.objects[i].update(bounding_boxes[j])
+                    indices_used_current.add(i)
+                    indices_used_new.add(j)
 
-            len_rows = distances.shape[0]
-            len_cols = distances.shape[1]
+            nr_of_current_objects = distances.shape[0]
+            nr_of_new_objects = distances.shape[1]
 
-            if len_rows > len_cols:
-                unused_rows = list(set(
-                    range(0, distances.shape[0])).difference(used_rows))
-                # print("Unused rows:", unused_rows)
-                for i in reversed(unused_rows):
+            if nr_of_current_objects > nr_of_new_objects:
+                # Finding the difference of sets == unused indices
+                # Converting to list in order to use reversed() later
+                indices_unused_current = list(set(
+                    range(nr_of_current_objects)).difference(indices_used_current))
+
+                # reversed() in order not to mess up the indexing
+                # (iteration + mutation on the same list)
+                for i in reversed(indices_unused_current):
                     obj = self.objects[i]
                     obj.has_disappeared()
                     self.delete_if_disappeared(obj)
-            elif len_rows < len_cols:
-                unused_cols = set(
-                    range(0, distances.shape[1])).difference(used_cols)
-                for j in unused_cols:
-                    self.new_object(world_points[j])
+            elif nr_of_current_objects < nr_of_new_objects:
+                indices_unused_new = set(
+                    range(nr_of_new_objects)).difference(indices_used_new)
+                for j in indices_unused_new:
+                    self.new_object(bounding_boxes[j])
 
         if VERBOSE:
-            print("Objects:", list(map(str, self.objects)))
+            rospy.loginfo("Objects: " + str(list(map(str, self.objects))))
 
     def get_position_dict(self):
         position_dict = {}
@@ -176,33 +208,43 @@ class Tracker:
 
 class RosTracker:
     def __init__(self):
-        self.points = []
+        self.bounding_boxes = []
         self.max_frames_disappeared = 30
-        self.tracker = Tracker(self.max_frames_disappeared)
-        self.predictor = predictor.Predictor(self.tracker, 0.4)
         self.time = time.time()
         self.framerate = 0
-        # For handling RViz visualization
-        self.rviz = rviz_util.RViz(frame_id="world")
 
-        self.frame_odom_id = "odom"
+        # Initializing the tracker
+        self.tracker = Tracker(self.max_frames_disappeared)
+
+        # Initializing the predictor
+        self.predictor = predictor.Predictor(self.tracker, sensitivity=0.2)
+
+        # Defining the frame IDs
+        self.frame_id_world = "world"
+        self.frame_id_odom = "odom"
+        self.frame_id_realsense = "realsense_mount"
+
+        # Creating the transform broadcasters
         pose_world = Pose()
         pose_world.orientation.w = 1
         self.tf_publisher_world = rviz_util.TFPublisher(
             pose_world, "world", "odom")
-        self.frame_realsense_id = "realsense_mount"
+
         pose_realsense = Pose()
-        # pose_realsense.orientation = Quaternion(0.5, -0.5, 0.5, -0.5)
         pose_realsense.orientation = Quaternion(0.5, -0.5, 0.5, -0.5)
-        # pose_realsense.orientation.w = 1
         pose_realsense.position.x = 0.17
         pose_realsense.position.z = 0.20
         self.tf_publisher = rviz_util.TFPublisher(
             pose_realsense, "base_link", "realsense_mount")
-
+        
+        # Creating the TF2 buffer and listener
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
+        # For handling RViz visualization
+        self.rviz = rviz_util.RViz(frame_id=self.frame_id_world)
+
+        # Optionally, creating a list of timers (for performance measurement)
         if TIME:
             self.timers = []
 
@@ -215,7 +257,7 @@ class RosTracker:
         )
 
         # Published topics
-        self.points_topic = "/ros_3d_bb/"
+        self.bounding_boxes_topic = "/ros_3d_bb/"
         self.marker_topic = "visualization_marker"
 
         # Publishers
@@ -225,6 +267,8 @@ class RosTracker:
             rospy.loginfo("Subscribed to topic: " + self.bb_point_topic)
 
     def update_framerate(self):
+        """Updates the program's framerate for making accurate predictions"""
+
         current_time = time.time()
         self.framerate = 1 / (current_time - self.time)
         self.time = current_time
@@ -234,35 +278,36 @@ class RosTracker:
             timer = Timer("update")
             self.timers.append(timer)
 
+        # Publishing the coordinate frames 
         self.tf_publisher.publish()
         self.tf_publisher_world.publish()
 
-        trans = self.tf_buffer.lookup_transform(
-            "world", self.frame_realsense_id, rospy.Time()
+        # Finding the transformation from the world to the RealSense camera,
+        # as the detected objects should be situated in the world frame.
+        transform = self.tf_buffer.lookup_transform(
+            self.frame_id_world, self.frame_id_realsense, rospy.Time()
         )
-        # print(trans)
 
-        self.points = []
+        if DEBUG:
+            rospy.loginfo(transform)
+
+        self.bounding_boxes = []
         for bounding_box in bb_array.boxes:
-            # self.points.append(
-            #     WorldPoint(bounding_box=bounding_box)
-            # )
-            pose_stamped = PoseStamped(
+            # "do_transform_pose" requires stamped poses, so converting the original to stamped
+            original_pose_stamped = PoseStamped(
                 bounding_box.header, bounding_box.center)
-            vector3_stamped = Vector3Stamped(
-                bounding_box.header, bounding_box.size)
-            pose_transformed = tf2_geometry_msgs.do_transform_pose(
-                pose_stamped, trans)
-            # vector3_transformed = tf2_geometry_msgs.do_transform_vector3(
-            #     vector3_stamped, trans)
+            transformed_pose_stamped = tf2_geometry_msgs.do_transform_pose(
+                original_pose_stamped, transform)
+            # Don't transform the size vector, as the pose is already transformed!
 
-            # new_bb = BoundingBox3D(
-            #     bounding_box.header, pose_transformed.pose, vector3_transformed.vector)
             new_bb = BoundingBox3D(
-                bounding_box.header, pose_transformed.pose, bounding_box.size)
-            print(new_bb)
-            self.points.append(
-                WorldPoint(bounding_box=new_bb)
+                bounding_box.header, transformed_pose_stamped.pose, bounding_box.size)
+
+            if VERBOSE:
+                rospy.loginfo(new_bb)
+
+            self.bounding_boxes.append(
+                BoundingBox(bounding_box=new_bb)
             )
 
         if TIME:
@@ -273,7 +318,7 @@ class RosTracker:
         if TIME:
             timer.update()
 
-        self.tracker.update(self.points)
+        self.tracker.update(self.bounding_boxes)
 
         if TIME:
             timer.update()
@@ -298,7 +343,7 @@ class RosTracker:
             y = obj.y / scaling
             height = obj.height / scaling
             diameter = obj.diameter / scaling
-            print("Diameter:", diameter)
+            rospy.loginfo("Diameter:" + str(diameter))
             v_x = x + obj.v_x / scaling * self.framerate
             v_y = y + obj.v_y / scaling * self.framerate
             duration = self.max_frames_disappeared / self.framerate
@@ -323,7 +368,7 @@ class RosTracker:
     def shutdown(self):
         if TIME:
             # for timer in self.timers:
-            #     print(timer)
+            #     rospy.loginfo(timer)
             averages = Timer.average_times(self.timers)
             rospy.loginfo("Average timings (in ms): " + str(averages))
         rospy.loginfo("Exiting...")
